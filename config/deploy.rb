ffdf151ceda13cef0c76cba139ddc2efdcbf2e8f
Douglas Rossignolli <douglas@kanamobi.com.br>
2015-03-31 14:08:37 -0300

    fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :user, "xshirowx"
 set :port, 22
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
-set :use_sudo, true
+set :use_sudo, false
 
 set :scm, "git"
 set :repository, "git@github.com:xdougx/ruby-everyday.git"
@@ -28,7 +28,7 @@ namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
     task command, roles: :app, except: {no_release: true} do
-      run "bash /home/#{user}/#{application}/current/config/unicorn_init.sh #{command}"
+      run "bash /home/#{user}/apps/#{application}/current/config/unicorn_init.sh #{command}"
       #run "/etc/init.d/unicorn_#{application} #{command}"
     end
   end
