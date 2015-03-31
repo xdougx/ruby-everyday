004a0aeaf99eb58ce0df7ce3c80999825ff5aae3
Douglas Rossignolli <douglas@kanamobi.com.br>
2015-03-31 10:58:14 -0300

    fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,6 @@ require "bundler/capistrano"
 require "rvm/capistrano"
 
 set :rails_env, 'production'
-set :user, "ubuntu"
+set :user, "xshirowx"
 server "104.167.113.131", :web, :app, :db, primary: true
 set :branch, "master"
