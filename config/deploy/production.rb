5970236e7f53f0207e92f45961f2a8dbd30004cc
Douglas Rossignolli <douglas.rossignolli@gmail.com>
2015-04-02 11:09:21 -0300

    Delete production.rb

--- config/deploy/production.rb
+++
@@ -1,7 +0,0 @@
-require "bundler/capistrano"
-require "rvm/capistrano"
-
-set :rails_env, 'production'
-set :user, "xshirowx"
-server "104.167.113.131", :web, :app, :db, primary: true
-set :branch, "master"
